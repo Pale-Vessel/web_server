17f86d6527de2d621571d9e6d0b7a5c0c3c7bb59
Vessel <waspsting@duck.com>
2025-10-19 12:42:09 +0100

    send site data

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![deny(clippy::unwrap_used)]
 
 use std::{
+    fs,
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
 };
@@ -20,11 +21,16 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let http_request: Vec<_> = buf_reader
         .lines()
-        .map(|result| result.expect("Couldn't read datastream"))
+        .map(|result| result.expect("failed to read line"))
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("site.html").expect("failed to read file");
+    let length = contents.len();
 
-    stream.write_all(response.as_bytes()).expect("failed to write");
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).expect("failed to write response to stream");
 }
