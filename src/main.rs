e943c97d13847067da2daae821b2daa01cc98b6c
Vessel <waspsting@duck.com>
2025-10-19 12:45:41 +0100

    rework response

--- src/main.rs
+++ src/main.rs
@@ -18,19 +18,22 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
+    let request_line = buf_reader
         .lines()
-        .map(|result| result.expect("failed to read line"))
-        .take_while(|line| !line.is_empty())
-        .collect();
+        .next()
+        .expect("Expected next line")
+        .expect("Failed to read line");
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("site.html").expect("Failed to read file");
+        let length = contents.len();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("site.html").expect("failed to read file");
-    let length = contents.len();
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-    stream
-        .write_all(response.as_bytes())
-        .expect("failed to write response to stream");
+        stream
+            .write_all(response.as_bytes())
+            .expect("failed to write response to stream");
+    } else {
+        todo!()
+    }
 }
