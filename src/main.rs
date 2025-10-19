ee76d0c82bc48b3bda9d5be3326848c68417f12f
Vessel <waspsting@duck.com>
2025-10-19 12:42:40 +0100

    format

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,6 @@ fn main() {
     }
 }
 
-
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let http_request: Vec<_> = buf_reader
@@ -29,8 +28,9 @@ fn handle_connection(mut stream: TcpStream) {
     let contents = fs::read_to_string("site.html").expect("failed to read file");
     let length = contents.len();
 
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).expect("failed to write response to stream");
+    stream
+        .write_all(response.as_bytes())
+        .expect("failed to write response to stream");
 }
