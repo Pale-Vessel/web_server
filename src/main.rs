49c22d63f728c019c6bc86f46f00e0b3e9ca1f83
Vessel <waspsting@duck.com>
2025-10-19 12:34:42 +0100

    replace unwrap with expect

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::net::TcpListener;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").expect("Failed to bind");
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        let stream = stream.expect("Failed to get stream");
 
         println!("Connection established!");
     }
